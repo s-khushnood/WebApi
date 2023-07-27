20ee03b2c41431b9129b64905b2f21b0e9f92aa8
s khushnood <138812257+s-khushnood@users.noreply.github.com>
2023-07-27 06:51:30 -0400

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace Web_API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var cors = new System.Web.Http.Cors.EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
