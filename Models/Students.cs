e0df2a64dc6a19ed716e5141e3c25006a6603632
s khushnood <138812257+s-khushnood@users.noreply.github.com>
2023-07-27 06:54:49 -0400

    Add files via upload

---
+++ Models/Students.cs
@@ -0,0 +1,13 @@
+﻿namespace Web_API.Models
+{
+    public class Students
+    {
+        public int studentId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string studentAddress { get; set; }
+        public string studentAge { get; set; }
+        public string phoneNumber { get; set; }
+
+    }
+}
